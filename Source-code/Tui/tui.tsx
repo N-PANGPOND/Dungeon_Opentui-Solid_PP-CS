ac9395a0abcf2978ada4a5ddd57f52653d91dc57
N-PANGPOND <pangpon252550@gmail.com>
2026-09-16 23:24:07 +0700

    add text Action

--- Source-code/Tui/tui.tsx
+++ Source-code/Tui/tui.tsx
@@ -92,6 +92,14 @@ const App = () => {
                     </box>
                     <box style={{ borderStyle: "rounded", flexDirection: "column", justifyContent: "space-between", width: `100%`, height: `30%`, borderColor: "#35f306" }}>
                         <text>Action :</text>
+                        <text>  "1" : Attack
+                                "2" : Strike
+                                "3" : UseItem
+                                "4" : Run
+                                "5" : Defend
+                                "6" : Counter
+                                "7" : UseItem
+                                "8" : Run </text>
                     </box>
                 </box>
             </box>
