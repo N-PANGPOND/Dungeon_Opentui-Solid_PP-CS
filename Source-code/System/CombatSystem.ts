16cbb1d87145f9479b5ff7d754e6c16c57e036b5
nongcho <chotiratxx@gmail.com>
2026-09-23 23:00:57 +0700

    NongCho wife (#31)
    Co-authored-by: N-PANGPOND <pangpon252550@gmail.com>

--- Source-code/System/CombatSystem.ts
+++ Source-code/System/CombatSystem.ts
@@ -9,12 +9,21 @@ type BattleOver = {"Over":boolean,"monster":Monster}
 export class CombatSystem {
     private monster: Monster;
     private isPlayerAttacker: boolean;
+    private hasFled: boolean = false;
     constructor(private player: Player,DungeonMap:DungeonMap, private ShowMessage: (log: logType) => void = () => {}) { 
         this.player = player;
         let distToExit: number = Math.abs(DungeonMap.getExitPos().x - this.player.getPosition().x) + Math.abs(DungeonMap.getExitPos().y - this.player.getPosition().y);
         this.monster = MonsterFactory.createMonster(distToExit);
         this.isPlayerAttacker = true;
+        
     }
+    public fleeWithSmokeBomb(): void {
+    this.hasFled = true;
+    }
+
+    public getHasFled(): boolean {
+    return this.hasFled;
+   }
     startbattle(playerAction:AttackingType | DefensiveType): void {
         if (!this.player.isDead() && !this.monster.isDead()) {
             if (this.isPlayerAttacker) {
@@ -41,7 +50,7 @@ export class CombatSystem {
     }
 
     isBattleOver(): BattleOver {
-        const Over : boolean = this.player.isDead() ? this.player.isDead() : this.monster.isDead()
+        const Over : boolean = this.player.isDead() || this.monster.isDead() || this.hasFled;
         return {"Over":Over,"monster":this.monster}
     }
 
