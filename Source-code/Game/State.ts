baefdd1209c1b000f7e96d170b8dcbf31ff7c976
N-PANGPOND <time61149@gmail.com>
2026-09-23 11:15:00 +0700

    fix Gamestate

--- Source-code/Game/State.ts
+++ Source-code/Game/State.ts
@@ -17,6 +17,7 @@ export class GameState {
   private isGetWife: boolean;
   private isPause: boolean;
   private combatSystem: CombatSystem;
+  private eventTriggeredTiles: Set<string> = new Set<string>();
  // private eventSystem: GameEvent;
 
   constructor(public currentMap: DungeonMap, private ShowMessage: (log: logType) => void = () => {}) {
@@ -70,20 +71,28 @@ export class GameState {
   }
 
   public checkTileEvent(): void {
-    const tileEvents : {
-      name:string,
-      weight:number,
-      action:() => void 
+  const key = `${this.player.Position.x},${this.player.Position.y}`;
+
+  if (this.eventTriggeredTiles.has(key)) {
+    return; 
+  } else {
+    this.eventTriggeredTiles.add(key);
+
+    const tileEvents: {
+      name: string;
+      weight: number;
+      action: () => void;
     }[] = [
-      {name : "monster",weight : 0.25,action : () => this.eventCombat()},
-      {name : "Trap",weight : 0.07,action : () => this.eventTrap()},
-      {name : "Treasure",weight : 0.10,action : () => this.eventTreasure()},
-      {name : "Potion",weight : 0.04,action : () => this.eventPotion()},
-      {name : "shop",weight : 0.09,action : () => this.eventShop()},
-      {name : "Nothing",weight : 0.50,action : () => this.eventNothing()}
-    ]
-    const totalWeight = tileEvents.reduce((sum, event) => sum + event.weight, 0)
-    let chance = Math.random()*totalWeight;
+      { name: "monster", weight: 0.25, action: () => this.eventCombat() },
+      { name: "Trap", weight: 0.07, action: () => this.eventTrap() },
+      { name: "Treasure", weight: 0.10, action: () => this.eventTreasure() },
+      { name: "Potion", weight: 0.04, action: () => this.eventPotion() },
+      { name: "shop", weight: 0.09, action: () => this.eventShop() },
+      { name: "Nothing", weight: 0.50, action: () => this.eventNothing() },
+    ];
+
+    const totalWeight = tileEvents.reduce((sum, event) => sum + event.weight, 0);
+    let chance = Math.random() * totalWeight;
 
     for (const event of tileEvents) {
       if (chance < event.weight) {
@@ -93,7 +102,7 @@ export class GameState {
       chance -= event.weight;
     }
   }
-
+}
   public eventTrap():void{
     const damage = Event.prototype.Trap(this.player)
     this.ShowMessage({type: "System" , text: `เจอกับดัก เสีย Hp ${damage}!!`})
